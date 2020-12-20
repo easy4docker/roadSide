8e0f47d616b68c7209d7fd074431138f31a3e0c6
b2blosangeles <b2blosangeles@gmail.com>
2020-12-20 14:54:23 -0800

    update

--- dockerSetting/_entrypointEngine.sh
+++ dockerSetting/_entrypointEngine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # /entrypoint.sh && mysqld
-echo "{\"MAIN_PORT\":\"$MAIN_PORT\",\"INIT_UI_TOKEN\":\"$INIT_UI_TOKEN\",\"INIT_UI_TOKEN\":\"$ROOT_PASSWORD\",\"ROOT_HOST\":\"$ROOT_HOST\"}" > /var/_masterInfo.json
+echo "{\"MAIN_PORT\":\"$MAIN_PORT\",\"INIT_UI_TOKEN\":\"$INIT_UI_TOKEN\",\"ROOT_PASSWORD\":\"$ROOT_PASSWORD\",\"ROOT_HOST\":\"$ROOT_HOST\"}" > /var/_masterInfo.json
 cd /var/_localApp
 node server.js
