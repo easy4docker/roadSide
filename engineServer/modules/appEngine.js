d95ab04f5fea820f6e566cd1c66166d69e95f30f
b2blosangeles <b2blosangeles@gmail.com>
2020-12-25 15:15:55 -0800

    Update appEngine.js

--- engineServer/modules/appEngine.js
+++ engineServer/modules/appEngine.js
@@ -55,7 +55,20 @@
 			} catch (e) {}
 			return tokens;
 		}
-
+		this.sendHeader = (filetype) => {
+			var me = this;
+			res.header("Access-Control-Allow-Origin", "*");
+			res.header("Access-Control-Allow-Headers", "X-Requested-With");
+			res.header('Access-Control-Allow-Headers', 'Content-Type'); 
+			if (filetype == 'js' || filetype == 'jsx' || filetype == 'vue') {
+				res.setHeader('Content-Type', "text/javascrip");
+			} else if (filetype == 'css') {
+				me.is_css = true;
+				res.setHeader('Content-Type', "text/css");
+			} else {
+				res.setHeader('Content-Type', "text/plain");
+			}			
+		}
 		this.runGet = ()=> {
 			var _tokens = me.getTokens();
 			var token=req.query.token;
@@ -72,6 +85,7 @@
 								res.sendFile(fn);
 							} else {
 								fs.readFile(fn, 'utf-8', (err, data)=> {
+									me.sendHeader(m[1]);
 									res.send((err) ? err.message : data);
 								});
 							}
