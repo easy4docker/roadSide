71feec83cd35639f7c54a6f912bd9de512c2871a
b2blosangeles <b2blosangeles@gmail.com>
2020-12-24 22:42:08 -0800

    Update appEngine.js

--- engineServer/modules/appEngine.js
+++ engineServer/modules/appEngine.js
@@ -24,7 +24,7 @@
 			} else {
 				const dirTree = pkg.require(env.root + '/vendor/directory-tree/node_modules/directory-tree');
 				const tree = dirTree(env.appMainFolder);
-				res.send(env);
+				res.send(tree);
 			}
 		}
 
