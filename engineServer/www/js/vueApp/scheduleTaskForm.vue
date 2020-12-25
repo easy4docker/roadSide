fbc4336ce20132eeb7da6361cc2ba93a15818a44
b2blosangeles <b2blosangeles@gmail.com>
2020-12-25 11:50:04 -0800

    Update scheduleTaskForm.vue

--- engineServer/www/js/vueApp/scheduleTaskForm.vue
+++ engineServer/www/js/vueApp/scheduleTaskForm.vue
@@ -1,6 +1,7 @@
 <template>
     <span class="form-group card p-1">
         <button class="btn btn-fluid btn-success btn-sm m-2" v-if="module!=='add'" v-on:click="addTask();">Add a task</button>
+        {{module}}
         <span v-if="module==='add'">
             <label>Add Your Task:
                 <button class="btn btn-fluid btn-success btn-sm m-2" v-on:click="$parent.submit();">Run</button>
