a62be25c702d1b956d51193f99407b6cf6d36063
b2blosangeles <b2blosangeles@gmail.com>
2020-12-25 12:00:43 -0800

    Update scheduleTaskForm.vue

--- engineServer/www/js/vueApp/scheduleTaskForm.vue
+++ engineServer/www/js/vueApp/scheduleTaskForm.vue
@@ -1,11 +1,11 @@
 <template>
     <span class="form-group card p-1">
-        <button class="btn btn-fluid btn-success btn-sm m-2" v-if="module!=='addTaskFrom'" v-on:click="switchModule('addTaskFrom');">Add a task</button>
+        <button class="btn btn-success btn-sm m-2" v-if="module!=='addTaskFrom'" v-on:click="switchModule('addTaskFrom');">Add a task</button>
         {{module}}
         <span v-if="module==='addTaskFrom'">
             <label>Add Your Task:
-                <button class="btn btn-success btn-sm m-2" v-on:click="$parent.submit();">Run</button>
-                <button class="btn btn-secondary btn-sm m-2" v-on:click="switchModule('');">Cancel</button>
+                <button class="btn btn-success btn-sm m-1" v-on:click="$parent.submit();">Run</button>
+                <button class="btn btn-secondary btn-sm m-1" v-on:click="switchModule('');">Cancel</button>
             </label>
             <textarea class="form-control" rows="2" id="query" v-model="form.command" v-on:keyup.enter="submit" 
             placeholder="Input shell command"></textarea>
