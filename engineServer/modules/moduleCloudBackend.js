077697e29c72b949d00e3e1ceb7f957bd6299d87
b2blosangeles <b2blosangeles@gmail.com>
2020-12-25 14:52:22 -0800

    Update moduleCloudBackend.js

--- engineServer/modules/moduleCloudBackend.js
+++ engineServer/modules/moduleCloudBackend.js
@@ -11,8 +11,14 @@
 				const tree = dirTree(env.appFolder + '/mainServer');
 				cbk(tree);
 			}
+			_f['scheduledTasks'] = (cbk) => {
+				const dirTree = pkg.require(env.root + '/vendor/directory-tree/node_modules/directory-tree');
+				const tree = dirTree(env.dataFolder + '/scheduledTasks');
+				cbk(tree);
+			}
+			
 			cp.serial(_f, (data) => {
-				res.send({localScripts : cp.data.localScripts});
+				res.send({localScripts : cp.data.localScripts, scheduledTasks : cp.scheduledTasks});
 			}, 6000);
 			
 		}
