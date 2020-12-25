dc33228fa6a2add8fca00e330989d6c957237574
b2blosangeles <b2blosangeles@gmail.com>
2020-12-25 11:59:40 -0800

    Update scheduleTaskForm.vue

--- engineServer/www/js/vueApp/scheduleTaskForm.vue
+++ engineServer/www/js/vueApp/scheduleTaskForm.vue
@@ -5,7 +5,7 @@
         <span v-if="module==='addTaskFrom'">
             <label>Add Your Task:
                 <button class="btn btn-success btn-sm m-2" v-on:click="$parent.submit();">Run</button>
-                <button class="btn btn-secondary btn-sm m-2" v-if="module!=='addTaskFrom'" v-on:click="switchModule('');">Cancel</button>
+                <button class="btn btn-secondary btn-sm m-2" v-on:click="switchModule('');">Cancel</button>
             </label>
             <textarea class="form-control" rows="2" id="query" v-model="form.command" v-on:keyup.enter="submit" 
             placeholder="Input shell command"></textarea>
