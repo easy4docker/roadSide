594f1ce02987b17628be7695ad79a782f1642bf0
b2blosangeles <b2blosangeles@gmail.com>
2020-12-25 09:42:58 -0800

    Update appBody.vue

--- engineServer/www/js/vueApp/appBody.vue
+++ engineServer/www/js/vueApp/appBody.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="card m-0">
+    <div class="card m-1">
         <div class="card-body card-list-section pt-0">
-            <div class="container-fluid mt-1 text-left">
+            <div class="container-fluid m-0 text-left">
                 <!--div class="container-fluid m-0"-->
                     <div class="row">
                         <div class="col-3 p-1">
