82796288811afa6dcd5e6738b7c6a82a54a856e5
b2blosangeles <b2blosangeles@gmail.com>
2020-12-25 00:09:28 -0800

    Update appBody.vue

--- engineServer/www/js/vueApp/appBody.vue
+++ engineServer/www/js/vueApp/appBody.vue
@@ -12,17 +12,17 @@
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-4 p-1">
-                            <div class="form-control alert-success text-center">
+                            <div class="form-control card alert-success text-center">
                                 Local Script
                             </div>
                         </div>
                         <div class="col-4 p-1">
-                            <div class="form-control text-center alert-warning">
+                            <div class="form-control card text-center alert-warning">
                                 Scheduled command
                             </div>
                         </div>
                         <div class="col-4 p-1">
-                            <div class="form-control text-center alert-info">
+                            <div class="form-control card text-center alert-info">
                                 Running Log
                             </div>
                         </div>
