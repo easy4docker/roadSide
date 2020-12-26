08bc71632b5713a6329b1ea3e3373f06a3cb94b8
b2blosangeles <b2blosangeles@gmail.com>
2020-12-25 19:40:20 -0800

    Update moduleCloudBackend.js

--- engineServer/modules/moduleCloudBackend.js
+++ engineServer/modules/moduleCloudBackend.js
@@ -19,8 +19,8 @@
 			}
 			_f['logs'] = (cbk) => {
 				const dirTree = pkg.require(env.root + '/vendor/directory-tree/node_modules/directory-tree');
-				const tree = dirTree(env.dataFolder + '/logs');
-				cbk((!tree) ? (env.dataFolder + '/logs') : tree.children);
+				const tree = dirTree(env.dataFolder + '/_log');
+				cbk((!tree) ? (env.dataFolder + '/log') : tree.children);
 			}
 			CP.serial(_f, (data) => {
 				res.send({localScripts : CP.data.localScripts, scheduledTasks : CP.data.scheduledTasks, logs : CP.data.logs});
