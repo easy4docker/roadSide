c6dd81a8d962deb018e7f3f01503f1ca5a244a24
b2blosangeles <b2blosangeles@gmail.com>
2020-12-26 08:49:57 -0800

    Update scheduleTaskForm.vue

--- engineServer/www/js/vueApp/scheduleTaskForm.vue
+++ engineServer/www/js/vueApp/scheduleTaskForm.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="card p-1 text-left p-2 scheduleFromSection alert-dark">
-        <span v-if="module!=='addTaskFrom'" >
+        <!--span v-if="module!=='addTaskFrom'" >
             Cron Tasks <button class="btn btn-success btn-sm right" v-on:click="switchModule('addTaskFrom');">Schedule a task</button>
-        </span>
-        <span v-if="module==='addTaskFrom'">
+        </span-->
+        <!--span v-if="module==='addTaskFrom'"-->
             <label>Add Your Task:
                 <button class="btn btn-success btn-sm m-1" :disabled="saveDisable()" v-on:click="saveTask();">Submit</button>
                 <button class="btn btn-secondary btn-sm m-1" v-on:click="switchModule('');">Cancel</button>
             </label>
             <textarea class="form-control" rows="2" id="query" v-model="form.command" v-on:keyup.enter="submit" 
             placeholder="Input shell command"></textarea>
-        </span>
+        <!--/span-->
     </div>
 </template>
 <script>
