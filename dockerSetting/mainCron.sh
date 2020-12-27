bd1f9357552b35b31c30dd7eaea3ebfecf1bb4c2
easy4docker <76085700+easy4docker@users.noreply.github.com>
2020-12-26 19:31:04 -0800

    Update mainCron.sh

--- dockerSetting/mainCron.sh
+++ dockerSetting/mainCron.sh
@@ -33,6 +33,8 @@ for f in "$CRON_PATH"/*; do
     echo $execfn > $markfile
     echo "-- Ran $f -- at $(date +"%m/%d/%Y %H:%M:%S")"
     mv -f $f $execfn || true
+    echo "==>"
+    sh $execfn
     sh $execfn >> $LOG_PATH/$(basename $f).log
     rm -fr $execfn || true
     rm -fr $markfile || true
