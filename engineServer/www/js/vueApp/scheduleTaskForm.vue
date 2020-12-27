531de1ff53049e7b9ba3118c5897dbc06a22401a
b2blosangeles <b2blosangeles@gmail.com>
2020-12-26 16:05:59 -0800

    Update scheduleTaskForm.vue

--- engineServer/www/js/vueApp/scheduleTaskForm.vue
+++ engineServer/www/js/vueApp/scheduleTaskForm.vue
@@ -1,16 +1,11 @@
 <template>
     <div class="card p-1 text-left p-2 scheduleFromSection alert-dark">
-        <!--span v-if="module!=='addTaskFrom'" >
-            Cron Tasks <button class="btn btn-success btn-sm right" v-on:click="switchModule('addTaskFrom');">Schedule a task</button>
-        </span-->
-        <!--span v-if="module==='addTaskFrom'"-->
-            <label>Add Your Task:
-                <button class="btn btn-success btn-sm m-1" :disabled="saveDisable()" v-on:click="saveTask();">Submit</button>
-                <button class="btn btn-secondary btn-sm m-1" v-on:click="switchModule('');">Cancel</button>
-            </label>
-            <textarea class="form-control" rows="2" id="query" v-model="form.command" v-on:keyup.enter="submit" 
-            placeholder="Input shell command"></textarea>
-        <!--/span-->
+        <label>Add Your Task:
+            <button class="btn btn-success btn-sm m-1" :disabled="saveDisable()" v-on:click="saveTask();">Submit</button>
+            <button class="btn btn-secondary btn-sm m-1" v-on:click="switchModule('');">Cancel</button>
+        </label>
+        <textarea class="form-control" rows="2" id="query" v-model="form.command" v-on:keyup.enter="submit" 
+        placeholder="Input shell command"></textarea>
     </div>
 </template>
 <script>
@@ -21,16 +16,12 @@ module.exports = {
             root : this.$parent.root,
             form : {
                 command : ''
-            },
-            module : ''
+            }
         }
     },
     mounted () {
     },
     methods :{
-        switchModule(v) {
-            this.module = v;
-        },
         saveDisable () {
             return (!this.form.command) ? true : false;
         },
@@ -43,7 +34,6 @@ module.exports = {
                 me.$parent.askBackendStatus();
             });
         },
-    
     },
     components: {
     }
