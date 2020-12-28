57a40cbcfcc9ab282f428e4018e2ad798b16adb4
easy4docker <easy4docker@gmail.com>
2020-12-28 15:03:31 -0800

    Update appBody.vue

--- engineServer/www/js/vueApp/appBody.vue
+++ engineServer/www/js/vueApp/appBody.vue
@@ -19,10 +19,6 @@
                             <a href="JavaScript:void(0)" v-on:click="passCommand(item.name)">{{item.name}}</a>
                         </div> 
                     </div>
-                    <!--div class="card p-1 text-center alert-success">
-                        <textarea class="form-control" rows="16" readonly>{{logs}}</textarea>
-                    </div-->
-
                 </div>
                 <div class="col-9 p-1">
                     <div class="card main_list_file_section p-1 alert-secondary" >
