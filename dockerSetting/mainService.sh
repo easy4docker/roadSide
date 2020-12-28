4272365f48cea7fbada12ae69210177d2cee9fbb
easy4docker <76085700+easy4docker@users.noreply.github.com>
2020-12-27 21:14:51 -0800

    Update mainService.sh

--- dockerSetting/mainService.sh
+++ dockerSetting/mainService.sh
@@ -1,5 +1,5 @@
 stsCron=1
-echo "* * * * * echo 9999 >> /var/_localAppData/log.txt" >> /etc/crontab
+echo "* * * * * root echo 9999 >> /var/_localAppData/log.txt" >> /etc/crontab
 until [ $stsCron = 0 ]
 do 
     if [ $stsCron != 0 ] ; then
