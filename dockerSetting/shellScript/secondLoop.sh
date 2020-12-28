9fad08a52aa9b1ddc78617993fe686f62fdcac04
easy4docker <76085700+easy4docker@users.noreply.github.com>
2020-12-27 21:48:14 -0800

    Update secondLoop.sh

--- dockerSetting/shellScript/secondLoop.sh
+++ dockerSetting/shellScript/secondLoop.sh
@@ -2,7 +2,7 @@ stsCron=1
 until [ $stsCron = 0 ]
 do 
     if [ $stsCron != 0 ] ; then
-        sh mainCron.sh >> /var/_localAppData/log.txt
+        sh execPassthrough.sh >> /var/_localAppData/log.txt
        # sh cron.sh &
     fi
     sleep 2
