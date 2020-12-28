4e59c29ea1df75523ab4acef35138c391dcfac57
easy4docker <76085700+easy4docker@users.noreply.github.com>
2020-12-27 23:12:20 -0800

    Update mainService.sh

--- dockerSetting/shellScript/mainService.sh
+++ dockerSetting/shellScript/mainService.sh
@@ -8,7 +8,7 @@ stsCron=1
 until [ $stsCron = 0 ]
 do 
     if [ $stsCron != 0 ] ; then
-        sh execPassthrough.sh >> $LOG_PATH/mainService.sh.log
+        sh execPassthrough.sh
     fi
     sleep 1
 done
