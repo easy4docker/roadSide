3a2067aa57479faa85b49f7aa971fa1a3c5856ae
easy4docker <76085700+easy4docker@users.noreply.github.com>
2020-12-27 22:04:22 -0800

    Delete secondLoop.sh

--- dockerSetting/shellScript/secondLoop.sh
+++
@@ -1,9 +0,0 @@
-stsCron=1
-until [ $stsCron = 0 ]
-do 
-    if [ $stsCron != 0 ] ; then
-        sh execPassthrough.sh >> /var/_localAppData/log.txt
-       # sh cron.sh &
-    fi
-    sleep 2
-done
