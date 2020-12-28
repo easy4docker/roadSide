dac4d3dabb695208f6f5f83501bef1efdecbc7c5
easy4docker <easy4docker@gmail.com>
2020-12-28 14:46:47 -0800

    Update scheduleTaskForm.vue

--- engineServer/www/js/vueApp/scheduleTaskForm.vue
+++ engineServer/www/js/vueApp/scheduleTaskForm.vue
@@ -2,7 +2,7 @@
     <div class="card p-1 text-left p-2 scheduleFromSection alert-dark">
         <label>Add Your Task:
             <button class="btn btn-success btn-sm m-1" :disabled="saveDisable()" v-on:click="saveTask();">Submit</button>
-            <button class="btn btn-secondary btn-sm m-1" v-on:click="switchModule('allTasks');">Cancel</button>
+            <button class="btn btn-secondary btn-sm m-1" v-on:click="cancel();">Cancel</button>
         </label>
         <textarea class="form-control" rows="2" id="query" v-model="$parent.cmd" v-on:keyup.enter="submit" 
         placeholder="Input shell command"></textarea>
