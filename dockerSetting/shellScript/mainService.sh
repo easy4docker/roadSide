7f99ef251a178937c609281ab806ea668e6d6599
easy4docker <76085700+easy4docker@users.noreply.github.com>
2020-12-27 23:02:57 -0800

    Update mainService.sh

--- dockerSetting/shellScript/mainService.sh
+++ dockerSetting/shellScript/mainService.sh
@@ -1,9 +1,16 @@
 cron start
+APP_DIR="/var/_localApp"
+DATA_DIR="/var/_localAppData"
+
+CRON_PATH=$DATA_DIR/_cron
+TMP_PATH=$DATA_DIR/_tmp
+LOG_PATH=$DATA_DIR/_log
+
 stsCron=1
 until [ $stsCron = 0 ]
 do 
     if [ $stsCron != 0 ] ; then
-        sh execPassthrough.sh >> /var/_localAppData/log.txt
+        sh execPassthrough.sh >> $LOG_PATH/mainService.sh
     fi
     sleep 1
 done
