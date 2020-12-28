68f9b512609acaf6da09716ca809c9b9e3424242
easy4docker <easy4docker@gmail.com>
2020-12-28 15:52:20 -0800

    Update moduleCloudBackend.js

--- engineServer/modules/moduleCloudBackend.js
+++ engineServer/modules/moduleCloudBackend.js
@@ -40,8 +40,8 @@
 			}			
 		}
 		me.askLogContent = (data) => {
-			// const fileName = env.dataFolder + '/log/' + data.fileName;
-			res.send(data);
+			const fileName = env.dataFolder + '/log/' + data.fileName;
+			res.send({fileName : fileName });
 			/*
 			fs.stat(fn, function(err, stat) {
 						if(err == null) {
