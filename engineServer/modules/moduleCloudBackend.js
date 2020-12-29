cdaca23670489d91bdcad2712abae3075cd96e84
easy4docker <easy4docker@gmail.com>
2020-12-29 09:06:20 -0800

    Update moduleCloudBackend.js

--- engineServer/modules/moduleCloudBackend.js
+++ engineServer/modules/moduleCloudBackend.js
@@ -105,7 +105,6 @@
 				} else {
 					// const cmd = 'echo "' + data.schedule + ' root (sh ' + fn + ') >> ' + env.dataFolder + '/_log/cron.log" >> /etc/cront ';
 					// const cmd = 'echo "_log/cron.log" >> /etc/cront ';
-					var str = 'cp ' + fn + ' /etc/';
 					exec('cp ' + fn + ' /etc/', {maxBuffer: 1024 * 2048},
 						function(error, stdout, stderr) {
 							cbk(true);
