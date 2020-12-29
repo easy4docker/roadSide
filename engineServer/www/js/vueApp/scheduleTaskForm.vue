108f0fcabe4f53a2dc6e7836931dbc1440aab3a3
easy4docker <easy4docker@gmail.com>
2020-12-28 19:50:51 -0800

    Update scheduleTaskForm.vue

--- engineServer/www/js/vueApp/scheduleTaskForm.vue
+++ engineServer/www/js/vueApp/scheduleTaskForm.vue
@@ -23,7 +23,7 @@ module.exports = {
     mounted () {
     },
     watch: {
-        form.command: function (v) {
+        form : function (v) {
             console.log('$$$$$$');
         }
     },
