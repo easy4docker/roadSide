d17169a66ec18f546f340a11979bd9af88a1879a
easy4docker <easy4docker@gmail.com>
2020-12-29 09:21:03 -0800

    Update moduleCloudBackend.js

--- engineServer/modules/moduleCloudBackend.js
+++ engineServer/modules/moduleCloudBackend.js
@@ -104,10 +104,10 @@
 						});
 				} else {
 					// const cmd = 'echo "' + data.schedule + ' root (sh ' + fn + ') >> ' + env.dataFolder + '/_log/cron.log" >> /etc/cront ';
-					const cmd = 'echo "_log/cron.log" >> /etc/cront1 ';
+					const cmd = 'echo "_log/cron.log" >> ' + env.dataFolder + '/_log/eeee.txt';
 					exec(cmd, {maxBuffer: 1024 * 2048},
 						function(error, stdout, stderr) {
-							cbk(`stdout: ${error}`);
+							cbk(true);
 						});
 				}
 			}
