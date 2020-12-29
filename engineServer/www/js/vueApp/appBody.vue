e4fcf69b75684df191979a293b4c2a0632655e6e
easy4docker <easy4docker@gmail.com>
2020-12-29 00:11:32 -0800

    Update appBody.vue

--- engineServer/www/js/vueApp/appBody.vue
+++ engineServer/www/js/vueApp/appBody.vue
@@ -85,7 +85,7 @@ module.exports = {
             const me = this;
             me.module = 'addTask'; 
             setTimeout(function() {
-                me.$refs.scheduleTaskForm.command = (!v) ? '' : ('python3 ' + v);
+                me.$refs.scheduleTaskForm.form.command = (!v) ? '' : ('python3 ' + v);
             }, 100);
         },
         showLog(v) {
