8efb0e391b81b759c968ad64f99c4532dd088b63
easy4docker <easy4docker@gmail.com>
2020-12-28 22:07:11 -0800

    Update commCronSchedule.vue

--- engineServer/www/js/vueApp/commCronSchedule.vue
+++ engineServer/www/js/vueApp/commCronSchedule.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="container-fluid m-1">
         <div class="row">
-            <div class="col-sm-6">Cron Schedule</div>
+            <div class="col-sm-3">Cron Schedule</div>
             <div class="col-sm-6"></div>
         </div>
         <div class="row">
-            <div class="col-sm-2"></div>
-            <div class="col-sm-10">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-9">
                 <div class="container-fluid m-1">
                     <div class="row text-center">
                         <div class="col-sm-2">MIN</div>
@@ -33,6 +33,7 @@
                     </div>
                 </div>
             </div>
+            <div class="col-sm-2"></div>
         </div>
     </div>
 </template>
