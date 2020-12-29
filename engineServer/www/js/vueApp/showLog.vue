7e28d2f5d9be5c9a80de1cbf28fac14943b0759e
easy4docker <easy4docker@gmail.com>
2020-12-29 11:51:53 -0800

    Update showLog.vue

--- engineServer/www/js/vueApp/showLog.vue
+++ engineServer/www/js/vueApp/showLog.vue
@@ -2,7 +2,9 @@
     <div class="card p-1 text-left p-2 scheduleFromSection alert-dark">
        <div class="m-2">
             Log {{fileName}}: 
-            <a href="JavaScript: void(0)" v-on:click="$parent.deleteFile('log', fileName)"><i class="fas fa-trash-alt"></i></a>
+            <a href="JavaScript: void(0)" v-on:click="$parent.deleteFile('log', fileName)">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </a>
        </div>
        <textarea class="form-control" rows="20" readonly>{{contents}}</textarea>
     </div>
