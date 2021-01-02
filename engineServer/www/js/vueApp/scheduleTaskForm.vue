f69596d9d7da04682cfc43758ea54003584f853f
easy4docker <easy4docker@gmail.com>
2021-01-01 17:59:22 -0800

    Update scheduleTaskForm.vue

--- engineServer/www/js/vueApp/scheduleTaskForm.vue
+++ engineServer/www/js/vueApp/scheduleTaskForm.vue
@@ -6,14 +6,16 @@
                 <input class="ml-3" type="radio" v-model="form.type" value="C"> Cron Task
             </label>
 
-            <label class="mt-2 p-1 mb-0">Task Name:</label>
-            <input type="text" class="form-control" v-model="form.name" placeholder="Input cron name">
+            <div class="p-2" v-if="form.type === 'C'">
+                <label class="mt-2 p-1 mb-0">Task Name:</label>
+                <input type="text" class="form-control" v-model="form.name" placeholder="Input cron name">
+            </div>   
+            <div class="p-2">
+                <label class="mt-2 p-1 mb-0">Task Command:</label>
+                <input type="text" class="form-control" v-model="form.command" placeholder="Input shell command">
 
-            <label class="mt-2 p-1 mb-0">Task Command:</label>
-            <input type="text" class="form-control" v-model="form.command" placeholder="Input shell command">
-
-            <comm-cron-schedule v-if="form.type === 'C'" v-bind:schedule.sync="form.schedule"></comm-cron-schedule>
-
+                <comm-cron-schedule v-if="form.type === 'C'" v-bind:schedule.sync="form.schedule"></comm-cron-schedule>
+            </div>
             <div class="p-3 text-right">
                 <button class="btn btn-success btn-sm m-1" :disabled="saveDisable()" v-on:click="saveTask();">Submit</button>
                 <button class="btn btn-secondary btn-sm m-1" v-on:click="cancel();">Cancel</button>
