6114af9adfc8fa30bc0d0575a3cda7aa9ef722e8
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-01-05 22:21:17 -0800

    Delete xp_1609484956799.sh

--- engineServer/xp_1609484956799.sh
+++
@@ -1,5 +0,0 @@
-echo "=== CRON RUN $(date +"%m-%d %H:%M:%S") ===" >> xp_1609484956799.sh/_log/cron.log ===
-cd /var/_localApp
-node test.js| sed 's/^/	>>	/' >> /var/_localAppData/_log/cron.log
-echo "	CRON Done $(date +"%m-%d %H:%M:%S") " >> xp_1609484956799.sh/_log/cron.log
-
