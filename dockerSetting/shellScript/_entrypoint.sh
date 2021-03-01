b34b1430e37e7c6b9d0907e166158537acef8ea7
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-02-28 20:38:43 -0800

    Update _entrypoint.sh

--- dockerSetting/shellScript/_entrypoint.sh
+++ dockerSetting/shellScript/_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/_localApp
-echo $shareFolderHost >> /var/_shareFolder/shareFolderHost.txt
+echo $onDemandCallbackHost >> /var/_shareFolder/onDemandCallbackHost.txt
 node test.js >> /var/_shareFolder/easy_ondemand.txt
 
 cp /var/shellScript/removeMe.json /var/_localAppData/commCron/
