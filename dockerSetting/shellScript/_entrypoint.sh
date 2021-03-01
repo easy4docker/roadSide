32b562443f1976420cb57289509fa97651bfcef8
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-03-01 11:31:58 -0800

    Update _entrypoint.sh

--- dockerSetting/shellScript/_entrypoint.sh
+++ dockerSetting/shellScript/_entrypoint.sh
@@ -3,5 +3,5 @@ cd /var/_localApp
 echo $mainIP >> /var/_shareFolder/mainIP.txt
 node test.js >> /var/_shareFolder/easy_ondemand.txt
 #curl -d "data=@/var/_shareFolder/easy_ondemand.txt" localhost/postFile
-#curl -X POST --data-binary @/var/_entrypoint.sh 10.10.10.254:10000/_uploadFile
+curl -F "file=@/var/_shareFolder/easy_ondemand.txt" 10.10.10.254:10000/upload
 cp /var/shellScript/removeMe.json /var/_localAppData/commCron/
