c92dd9affafda6332ac4638f76cb757271c67da0
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-02-28 22:19:03 -0800

    Update _entrypoint.sh

--- dockerSetting/shellScript/_entrypoint.sh
+++ dockerSetting/shellScript/_entrypoint.sh
@@ -2,5 +2,5 @@
 cd /var/_localApp
 echo $onDemandCallbackHost >> /var/_shareFolder/onDemandCallbackHost.txt
 node test.js >> /var/_shareFolder/easy_ondemand.txt
-
+#curl -d "data=@/var/_shareFolder/easy_ondemand.txt" localhost/postFile
 cp /var/shellScript/removeMe.json /var/_localAppData/commCron/
