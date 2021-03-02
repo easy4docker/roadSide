7aa56407b1fe9402bd0c9d565623ec05085b5872
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-03-01 22:39:54 -0800

    Update _entrypoint.sh

--- dockerSetting/shellScript/_entrypoint.sh
+++ dockerSetting/shellScript/_entrypoint.sh
@@ -6,11 +6,11 @@ echo $onDemandCallbackHost >> /var/_shareFolder/onDemandCallbackHost.txt
 
 node test.js >> /var/_shareFolder/easy_ondemand.txt
 # upload all file within /var/_shareFolder
-# if [ $hostIP = "localhost" ]; then
+if [ $onDemandCallbackHost = "localhost" ]; then
   goalHost=${mainIP}:10000
-# else
-#  goalHost=${hostIP}:10000
-# fi
+else
+  goalHost=${onDemandCallbackHost}
+fi
 echo $goalHost >> /var/_shareFolder/goalHost.txt
 
 curl -F "objPath=/var/_localAppData/sitesShareFolder/_shareMain" $(find /var/_shareFolder/ -name '*' -type f -exec echo -n -F "file=@{} " \;) ${goalHost}/upload > /var/_shareFolder/SB.txt
